0c5d3eb47ea91651b217b49ae31e706b557ca0e7
Shykoz <roma.chekmak123@gmail.com>
2023-12-08 18:04:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int secondNumber = 4;
+            Console.WriteLine(PowMethod(number, secondNumber));
+            Console.ReadLine();
+        }
+        public static int PowMethod(int a, int b)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result = result * a;
+            }
+            return result;
+        }
+    }
+}
